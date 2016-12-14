52ee57676fee7e54ada4e01c69210becacc6ea14
Mohammed Alsayyari <M.k.Alsayyari@gmail.com>
2016-12-14 13:00:44 +0300

    Drafted a database and local storage

--- www/database.sql
+++ www/database.sql
@@ -3,8 +3,39 @@ This file creates the databse. It has to be run exactly once or all data would
 be deleted by the drop table commands.
 */
 
-drop table if exists users;
-drop table if exists bottles;
-drop table if exists bags;
-drop table if exists mugs;
-drop table if exists stat;
+drop table if exists scan;
+drop table if exists member;
+drop table if exists bottle;
+drop table if exists bag;
+drop table if exists mug;
+
+create table member(
+  token text primary key,
+  name text not null,
+  points int,
+  created date
+);
+
+create table bottle(
+  id int primary key,
+  created timestamp with time zone,
+  location text
+);
+
+create table bag(
+  id int primary key,
+  created timestamp with time zone,
+  location text
+);
+
+create table mug(
+  id int primary key,
+  created timestamp with time zone,
+  location text
+);
+
+create table scan(
+  token text primary key references member(token),
+  id int,
+  type text
+);
