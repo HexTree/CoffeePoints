0f19429e17030184ee53b0b337e134db3d5376a1
Mohammed Alsayyari <M.k.Alsayyari@gmail.com>
2016-12-25 23:41:11 +0300

    fixed some issues after merge down
    # Conflicts:
    #	config.xml
    #	platforms/android/.gradle/2.14.1/taskArtifacts/cache.properties
    #	platforms/android/.gradle/2.14.1/taskArtifacts/cache.properties.lock
    #	platforms/android/.gradle/2.14.1/taskArtifacts/fileHashes.bin
    #	platforms/android/.gradle/2.14.1/taskArtifacts/fileSnapshots.bin
    #
    platforms/android/.gradle/2.14.1/taskArtifacts/fileSnapshotsToTreeSnapsh
    otsIndex.bin
    #	platforms/android/.gradle/2.14.1/taskArtifacts/taskArtifacts.bin
    #
    platforms/android/.gradle/2.14.1/tasks/_CordovaLib_compileDebugJavaWithJ
    avac/localClassSetAnalysis/localClassSetAnalysis.lock
    #
    platforms/android/.gradle/2.14.1/tasks/_CordovaLib_compileDebugJavaWithJ
    avac/localJarClasspathSnapshot/localJarClasspathSnapshot.lock
    #
    platforms/android/.gradle/2.14.1/tasks/_CordovaLib_compileReleaseJavaWit
    hJavac/localClassSetAnalysis/localClassSetAnalysis.lock
    #
    platforms/android/.gradle/2.14.1/tasks/_CordovaLib_compileReleaseJavaWit
    hJavac/localJarClasspathSnapshot/localJarClasspathSnapshot.lock
    #
    platforms/android/.gradle/2.14.1/tasks/_compileDebugJavaWithJavac/localC
    lassSetAnalysis/localClassSetAnalysis.bin
    #
    platforms/android/.gradle/2.14.1/tasks/_compileDebugJavaWithJavac/localC
    lassSetAnalysis/localClassSetAnalysis.lock
    #
    platforms/android/.gradle/2.14.1/tasks/_compileDebugJavaWithJavac/localJ
    arClasspathSnapshot/localJarClasspathSnapshot.lock
    #
    platforms/android/CordovaLib/build/intermediates/incremental/packageDebu
    gResources/compile-file-map.properties
    #
    platforms/android/CordovaLib/build/intermediates/incremental/packageRele
    aseResources/compile-file-map.properties
    #	platforms/android/CordovaLib/build/outputs/aar/CordovaLib-debug.aar
    #	platforms/android/CordovaLib/build/outputs/aar/CordovaLib-release.aar
    #	platforms/android/android.json
    #	platforms/android/assets/www/cordova_plugins.js
    #	platforms/android/assets/www/css/index.css
    #	platforms/android/assets/www/index.html
    #	platforms/android/assets/www/scan.html
    #	platforms/android/platform_www/cordova_plugins.js
    #	platforms/android/res/xml/config.xml
    #	platforms/browser/browser.json
    #	platforms/browser/build/package.zip
    #	platforms/browser/config.xml
    #	platforms/browser/platform_www/cordova_plugins.js
    #	platforms/browser/www/config.xml
    #	platforms/browser/www/cordova_plugins.js
    #	platforms/browser/www/css/index.css
    #	platforms/browser/www/index.html
    #	platforms/browser/www/scan.html
    #
    platforms/ios/CordovaLib/CordovaLib.xcodeproj/xcuserdata/7mTo.xcuserdata
    d/xcschemes/CordovaLib.xcscheme
    #	platforms/ios/Sustainability App.xcodeproj/project.pbxproj
    #	platforms/ios/Sustainability App/config.xml
    #	platforms/ios/ios.json
    #	platforms/ios/platform_www/cordova_plugins.js
    #	platforms/ios/www/cordova_plugins.js
    #	platforms/ios/www/css/index.css
    #	platforms/ios/www/index.html
    #	platforms/ios/www/scan.html
    #	platforms/platforms.json
    #	plugins/android.json
    #	plugins/browser.json
    #	plugins/fetch.json
    #	plugins/ios.json
    #	www/css/index.css
    #	www/index.html
    #	www/scan.html

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,35 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-geolocation.geolocation",
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.PositionError",
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-geolocation": "2.4.1",
+    "cordova-plugin-whitelist": "1.3.1",
+    "phonegap-plugin-barcodescanner": "6.0.2"
+};
+// BOTTOM OF METADATA
+});
