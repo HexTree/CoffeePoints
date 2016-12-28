3a0c6988e786ac4f0db3b9e3be6425847bbb3f4a
Mohammed Alsayyari <M.k.Alsayyari@gmail.com>
2016-12-28 18:24:14 +0300

    Added and removed plugin with minor fixes and clean ups

--- www/js/id.js
+++ www/js/id.js
@@ -1,6 +1,42 @@
 /* This script will handle the authentication of the identity of the users
  *
  */
+ //KAUST ID scan
+ //Mechanism for logging in via scanning an id
+ function scanKAUST()
+ {
+   cordova.plugins.barcodeScanner.scan
+   (
+     function(result)
+     {
+       if(result.format == "CODE_39"){
+           /*navigator.notification.prompt(
+               'Please enter your name',  // message
+               function (input){
+                 window.localStorage.setItem("Name", input.input1);
+               },                  // callback to invoke
+               'Registration',            // title
+               ['Ok','Call me Greeny'],             // buttonLabels
+               'Greeny Kaustian'                 // defaultText
+           );*/
+           var value = result.text;
+           window.localStorage.setItem("Loggedin", "True");
+           window.localStorage.setItem("Type", "KAUST");
+           window.localStorage.setItem("Identity", result.text);
+           window.location.href = 'home.html';
+       }
+       else if (!result.cancelled) {
+         document.getElementById("message").innerHTML = "Message: You are scanning a " + result.format + ". Please scan a barcode, (They are at the back of your card!)";
+       }
+     },
+     function(error)
+     {
+       //error callback
+       document.getElementById("message").innerHTML = "Error: " + error;
+     }
+   );
+ }
+
  //Google Auth
  function googleSignIn(){
    window.plugins.googleplus.login({
